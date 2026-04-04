5cff76f2255ec15c0b7fb84936c7ddeec8ca883e
xZuckert <caiquezuckert321@gmail.com>
2026-04-04 16:35:22 -0300

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/caique/vehicleapi/VehicleManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.caique.vehicleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleManagementApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/caique/vehicleapi/VehicleManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caique.vehicleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
